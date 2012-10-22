0c3a75d6a168ce7044204059696ca39e9af14f83
catseye <cpressey@gmail.com>
2012-10-22 09:06:23 +0100

    Move alpaca.pl to impl.  Add stub of new ref. impl., in Python.
    --HG--
    rename : src/Alpaca.pm => impl/alpaca.pl/Alpaca.pm
    rename : src/alpaca.pl => impl/alpaca.pl/alpaca.pl

---
+++ impl/alpaca.pl/README.markdown
@@ -0,0 +1,7 @@
+`alpaca.pl`
+===========
+
+This directory contains `alpaca.pl`, an implementation of ALPACA version 0.93.
+Since this is no longer the current version of ALPACA, this is no longer the
+reference implementation of the language.  It is retained here for historical
+interest and/or minimal continued support for existing ALPACA descriptions.
---
+++ src/alpaca/main.py
@@ -0,0 +1,41 @@
+"""
+alpaca [-at] description.alp
+
+Reference implementation of the ALPACA cellular automaton definition language,
+version 1.0-PRE.
+
+"""
+
+from optparse import OptionParser
+import random
+import re
+import sys
+
+
+def main(argv):
+    optparser = OptionParser(__doc__.strip())
+    optparser.add_option("-a", "--show-ast",
+                         action="store_true", dest="show_ast", default=False,
+                         help="show parsed AST instead of evaluating")
+    optparser.add_option("-t", "--test",
+                         action="store_true", dest="test", default=False,
+                         help="run test cases and exit")
+    (options, args) = optparser.parse_args(argv[1:])
+    if options.test:
+        import doctest
+        (fails, something) = doctest.testmod()
+        if fails == 0:
+            print "All tests passed."
+            sys.exit(0)
+        else:
+            sys.exit(1)
+    file = open(args[0])
+    text = file.read()
+    file.close()
+    #p = Parser(text)
+    #prog = p.program()
+    #if options.show_ast:
+    #    from pprint import pprint
+    #    pprint(prog)
+    #    sys.exit(0)
+    sys.exit(0)
