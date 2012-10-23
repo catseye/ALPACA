e6302d01aefd4241e1720a6de67ae2f54b0af1ae
catseye <cpressey@gmail.com>
2012-10-23 09:17:52 +0100

    Beginnings of parser.

---
+++ src/alpaca/parser.py
@@ -0,0 +1,111 @@
+from alpaca.scanner import Scanner
+
+"""
+AlpacaProgram   ::= Entries ("." | "begin" initial-configuration).
+Entries         ::= Entry {";" Entry}.
+Entry           ::= ClassDefinition
+                  | StateDefinition
+                  | NeighbourhdDef.
+ClassDefinition ::= "class" ClassID {MembershipDecl}
+                    [Rules].
+StateDefinition ::= "state" StateID {ReprDecl} {MembershipDecl}
+                    [Rules].
+NeighbourhdDef  ::= "neighbourhood" NeighbourhoodID
+                    Neighbourhood.
+
+ClassID         ::= identifier.
+StateID         ::= identifier.
+NeighbourhoodID ::= identifier.
+ReprDecl        ::= quoted-char
+                  | identifier ":" quoted-string.
+
+MembershipDecl  ::= ClassReferent.
+ClassReferent   ::= "is" ClassID.
+
+Rules           ::= Rule {"," Rule}.
+Rule            ::= "to" StateReferent "when" Expression.
+
+StateReferent   ::= StateID
+                  | arrow-chain
+                  | "me".
+
+Expression      ::= Term {("and" | "or" | "xor") Term}.
+Term            ::= AdjacencyFunc
+                  | "(" Expression ")"
+                  | "not" Term
+                  | BoolPrimitive
+                  | RelationalFunc.
+RelationalFunc  ::= StateReferent (["="] StateReferent | ClassReferent).
+AdjacencyFunc   ::= natural-number ["in" (Neigbourhood | NeighbourhoodID)]
+                    (StateReferent | ClassReferent).
+BoolPrimitive   ::= "true" | "false" | "guess".
+
+Neighbourhood   ::= "(" {arrow-chain} ")".
+"""
+
+class Parser(object):
+    def __init__(self, text):
+        self.scanner = Scanner(text)
+
+    def program(self):
+        entries = []
+        entries.append(self.entry())
+        while self.scanner.consume(';'):
+            entries.append(self.entry())
+        if self.scanner.consume('begin'):
+            # XXX read playfield
+            pass
+        else:
+            self.scanner.expect('.')
+        return entries
+
+    def entry(self):
+        if self.scanner.on('state'):
+            return self.state_defn()
+        elif self.scanner.on('class'):
+            return self.class_defn()
+        elif self.scanner.on('neighbourhood'):
+            return self.neighbourhood_defn()
+        else:
+            # XXX complain
+            pass
+    
+    def state_defn(self):
+        self.scanner.expect('state')
+        id = self.scanner.consume_type('identifier')
+        # while it's a quoted string or... something else,
+        # consume that
+        classes = []
+        while self.scanner.consume('is'):
+            classes.append(self.scanner.consume_type('identifier'))
+        rules = self.rules()
+        return AST('StateDefn', value=id, rules)
+
+    def class_defn(self):
+        self.scanner.expect('class')
+        id = self.scanner.consume_type('identifier')
+        classes = []
+        while self.scanner.consume('is'):
+            classes.append(self.scanner.consume_type('identifier'))
+        rules = self.rules()
+        return AST('ClassDefn', value=id, rules)
+
+    def neighbourhood_defn(self):
+        self.scanner.expect('neighbourhood')
+        id = self.scanner.consume_type('identifier')
+        n = self.neighbourhood()
+        return AST('NeighbourhoodDefn', value=id, [n])
+
+    def rules(self):
+        r = []
+        r.append(self.rule())
+        while self.scanner.consume(','):
+            r.append(self.rule())
+        return r
+
+    def rule(self):
+        self.scanner.expect('to')
+        s = self.state_ref()
+        self.scanner.expect('when')
+        e = self.bool_expr()
+        return AST('Rule', [s, e])
