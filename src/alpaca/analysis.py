e8e70059de97f88ba78a8cc87e436aa5a361ba90
Chris Pressey <cpressey@catseye.tc>
2018-01-25 16:49:29 +0000

    Fix bug: when ascending class hierarchy, get parent of correct class.

--- src/alpaca/analysis.py
+++ src/alpaca/analysis.py
@@ -58,7 +58,7 @@ def class_defn_is_a(alpaca, class_ast, class_id, verbose=False):
         assert class_decl.type == 'ClassDecl'
         if class_id == class_decl.value:
             return True
-        parent_class_ast = find_class_defn(alpaca, class_id)
+        parent_class_ast = find_class_defn(alpaca, class_decl.value)
         if class_defn_is_a(alpaca, parent_class_ast, class_id, verbose=verbose):
             return True
     return False
